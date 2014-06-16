bf002a021a3b40dc39fe2e9bf6f0e26bda7a4086
DeliriumNocturnum <jake@schwartzpub.com>
2014-06-16 12:11:45 -0500

    Quick Windows Install, partially tested

---
+++ Quick_Setup_Windows/README.md
@@ -0,0 +1,33 @@
+Quick Setup Scripts for myTurkDB
+========
+
+These are platform-specific scripts to help setup an user's environment for myTurkDB
+
+### Quick Setup on Windows 7
+
+##### Requisites
+
++ An OS running Windows 7 or later
++ Python 2.7.6 -- https://www.python.org/ftp/python/2.7.6/python-2.7.6.msi 
++ MySQL 5.6 -- http://dev.mysql.com/downloads/file.php?id=452278
++ MySQL-Python 1.2.3 or later -- http://www.codegood.com/download/10/
+
+##### Installation
+
+The quick setup script for Windows can be run from the same directory that you have downloaded the Requisites into from elevated cmd: quick_install.bat
+
+The script will automatically install MySQL Server 5.6.
+
+The script will automatically install Python 2.7.6.
+
+The script will automatically install MySQL-Python 1.2.3 or later, depending on your download.  You will need to simply click "next" and "finish" during the install.
+
+The script will set up the tables.
+
+
+##### Tested Versions and known issues
+
+Quick Setup for Windows has been tested on
+
++ Windows 7 Professional x64 - Python 2.7.6
+
---
+++ Quick_Setup_Windows/quick_install.bat
@@ -0,0 +1,55 @@
+@echo off
+
+@echo Setting up MySQL Installer
+
+START /wait msiexec /i mysql-installer-community-5.6.19.0.msi /quiet
+
+@echo Installing MySQL 5.6
+
+START /wait "" "C:\Program Files (x86)\MySQL\MySQL Installer\MySQLInstallerConsole.exe" --config=mysql-server-5.6-win32:passwd=root --product=* --catalog=mysql-5.6-win32 --action=install --type=full
+
+SET PATH=%PATH%;"C:\Program Files (x86)\MySQL\MySQL Server 5.6\bin"
+
+@echo Finished installing MySQL!
+
+@echo Installing Pyrthon 2.7.6
+
+START /wait msiexec /i python-2.7.6.msi /passive
+
+SET PATH=%PATH%;C:\Python27\
+
+@echo Finished installing Python!
+
+@echo Installing PIP
+
+python get-pip.py
+
+@echo Finished installing PIP
+
+SET PATH=%PATH%;C:\Python27\scripts\
+
+@echo Installing Beautiful Soup Module
+
+pip install beautifulsoup4
+
+@echo Installing Mechanize Module
+
+pip install mechanize
+
+@echo Installing MySQL-Python Module
+
+START /wait MySQL-python-1.2.5.win32-py2.7.exe
+
+mysql -uroot -proot -e "CREATE DATABASE mturk"
+
+mysql -uroot -proot -e "GRANT USAGE ON *.* to mturk@localhost IDENTIFIED BY 'mturk'"
+
+mysql -uroot -proot -e "GRANT ALL PRIVELEGES ON mturk.* TO mturk@localhost"
+
+mysql -uroot -proot -e "CREATE TABLE hitdb (hitId VARCHAR(100), date DATE, requesterName VARCHAR(100), requesterId VARCHAR(50), title VARCHAR(200), reward FLOAT, status VARCHAR(100), feedback VARCHAR(200), workerId VARCHAR(50))"
+
+mysql -uroot -proot -e "ALTER TABLE hitdb ADD PRIMARY KEY (hitId)"
+
+mysql -uroot -proot -e "CREATE TABLE workerdb (workerId VARCHAR(50), workerName VARCHAR(50), bonus FLOAT, transfer FLOAT)"
+
+mysql -uroot -proot -e "ALTER TABLE workerdb ADD PRIMARY KEY (workerId)"
